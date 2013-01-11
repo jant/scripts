70ce49a5bd82d9ed0623c8bbf61f1a643d78409c
jant <tjanousek@volny.cz>
2013-01-11 21:03:22 +0100

    a

--- contract_data.rb
+++ contract_data.rb
@@ -1,7 +1,9 @@
 # skript vygeneruje ze zadaneho csv souboru a symbolu vice csv souboru pro vsechny obsazene kontraktni mesice
-
+# a vygeneruje i seznam symbolu kontraktu oddeleny carkami
+# priklad volani: ruby contract_data.rb c:/hd/hd.txt ZW
 
 require "CSV"
+
 
 def check_usage
   unless ARGV.length == 2
@@ -10,13 +12,53 @@ def check_usage
   end
 end
 
+MONTHS = {
+    'F'=>'01',
+    'G'=>'02',
+    'H'=>'03',
+    'J'=>'04',
+    'K'=>'05',
+    'M'=>'06',
+    'N'=>'07',
+    'Q'=>'08',
+    'U'=>'09',
+    'V'=>'10',
+    'X'=>'11',
+    'Z'=>'12'
+}
+
+def create_filename(contract)
+  contract[0..1] + MONTHS[contract[2]] + contract[3..4] + '.Last.txt'
+end
+
 check_usage
 
-CSV.open('c:/HD/ZW/ZW 05-13.Last.txt', 'wb') do |output|
-  CSV.foreach(ARGV[0]) do |row|
-    if row[0] == 'ZWK13'
-      output << [row[2], row[3], row[4], row[5], row[6], row[7]]
-    end
+input_file_path = ARGV[0]
+symbol = ARGV[1]
+input_dirname = File.dirname(input_file_path)
+output_dir_path = File.join(input_dirname, symbol)
+Dir.mkdir(output_dir_path) if !File.exists?(output_dir_path)
+Dir.chdir(output_dir_path)
+
+soubory = Hash.new
+
+CSV.foreach(input_file_path) do |row|
+  contract = row[0]
+  if contract =~ Regexp.new(symbol) and contract.length == 5
+    if !soubory.has_key?(contract)
+      output = CSV.open(create_filename(contract), 'wb')
+      soubory.store(contract, output) 
+    else
+      output = soubory[contract]
+    end  
+    output << [row[2], row[3], row[4], row[5], row[6], row[7]]
+  end
+end
+
+File.open(File.join(input_dirname, symbol + '_symbols.txt'), 'wb') do |file|
+  soubory.each do |key, csv|
+    file << key[0..1] + MONTHS[key[2]] + key[3..4] + ','
+    csv.close
   end
 end
 
